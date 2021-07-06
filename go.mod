1291d5ee10da9baac1d508ce670bac0c8ae4043f
kuritka <kuritka@gmail.com>
2021-07-06 15:55:41 +0200

    v0.0.8

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/kuritka/doge-action
 
 go 1.16
-
-require github.com/AbsaOSS/gopkg v0.1.2
--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
-	"github.com/AbsaOSS/gopkg/env"
+	"os"
 )
 
 const clusterName = "CLUSTER_NAME"
 
 func main(){
 	fmt.Println("Hello from DOGE 🐩🐩 action")
-	x := env.GetEnvAsStringOrFallback(clusterName, "ERROR")
+	x := GetEnvAsStringOrFallback(clusterName, "ERROR")
 	fmt.Println(x)
+}
+
+
+
+// GetEnvAsStringOrFallback returns the env variable for the given key
+// and falls back to the given defaultValue if not set
+func GetEnvAsStringOrFallback(key, defaultValue string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return defaultValue
 }
